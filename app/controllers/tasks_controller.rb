32f89adbad7062c3831d8f5d9c2f73e304785be8
afonso miguel <afonso.miguel1995@gmail.com>
2022-02-15 15:32:10 +0000

    corrected type in set_task method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_tasks, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
@@ -34,7 +34,7 @@ class TasksController < ApplicationController
 
   private
 
-  def set_tasks
+  def set_task
     @task = Task.find(params[:id])
   end
 
